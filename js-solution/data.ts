a7d713d24549b989c77b5e82a5fe1c5f10d0c28f
Vanessa Viray <161077888+vanessaviray@users.noreply.github.com>
2024-05-24 09:17:52 -0700

    Initial commit

---
+++ js-solution/data.ts
@@ -0,0 +1,72 @@
+export type UnsavedEntry = {
+  title: string;
+  notes: string;
+  photoUrl: string;
+};
+export type Entry = UnsavedEntry & {
+  entryId: number;
+};
+
+type Data = {
+  entries: Entry[];
+  nextEntryId: number;
+};
+
+const dataKey = 'code-journal-data';
+
+function readData(): Data {
+  let data: Data;
+  const localData = localStorage.getItem(dataKey);
+  if (localData) {
+    data = JSON.parse(localData);
+  } else {
+    data = {
+      entries: [],
+      nextEntryId: 1,
+    };
+  }
+  return data;
+}
+
+function writeData(data: Data): void {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem(dataKey, dataJSON);
+}
+
+export async function readEntries(): Promise<Entry[]> {
+  return readData().entries;
+}
+
+export async function readEntry(entryId: number): Promise<Entry | undefined> {
+  return readData().entries.find((e) => e.entryId === entryId);
+}
+
+export async function addEntry(entry: UnsavedEntry): Promise<Entry> {
+  const data = readData();
+  const newEntry = {
+    ...entry,
+    entryId: data.nextEntryId++,
+  };
+  data.entries.unshift(newEntry);
+  writeData(data);
+  return newEntry;
+}
+
+export async function updateEntry(entry: Entry): Promise<Entry> {
+  const data = readData();
+  const newEntries = data.entries.map((e) =>
+    e.entryId === entry.entryId ? entry : e
+  );
+  data.entries = newEntries;
+  writeData(data);
+  return entry;
+}
+
+export async function removeEntry(entryId: number): Promise<void> {
+  const data = readData();
+  const updatedArray = data.entries.filter(
+    (entry) => entry.entryId !== entryId
+  );
+  data.entries = updatedArray;
+  writeData(data);
+}
